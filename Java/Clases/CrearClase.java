307e71cfaccf62cbf4445d56e972aff6418b361b
BelenBRC <belenrob.alumn@cescristorey.com>
2024-10-10 11:24:51 +0200

    Explicación de instanciación y uso de una clase

---
+++ Java/Clases/CrearClase.java
@@ -0,0 +1,11 @@
+public class CrearClase {
+    //Esto es una clase
+    int facilidad = 10; //Facilidad 10/10
+
+    public static void main(String[] args){
+        //Creamos una instancia de CrearClase
+        CrearClase instancia = new CrearClase();
+        //Leemos el valor de la variable
+        System.out.println(instancia.facilidad);
+    }
+}
